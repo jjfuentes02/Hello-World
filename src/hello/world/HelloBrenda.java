4d11356504b055da53c37adb35f043b16fb558d2
Jeovany Fuentes <jeofuentes33@gmail.com>
2018-03-22 19:59:36 -0400

    removing file and updating HelloWorld

--- src/hello/world/HelloBrenda.java
+++
@@ -1,8 +0,0 @@
-package hello.world;
-
-public class HelloBrenda {
-	public static void main(String[] args) {
-		System.out.println("Hello World: Jeovany");
-		System.out.println("Hello World: Brenda");
-	}
-}
